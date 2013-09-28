b894e25fe5c4973b4aa7964bee1d877df869e4ec
josh_smith <jsmith.in.jax@gmail.com>
2013-09-28 15:19:05 -0400

    add support for MD5 hashes on the email as required by gravatar to request images

--- spades/spades/Hubs/ChatHub.cs
+++ spades/spades/Hubs/ChatHub.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNet.SignalR;
+﻿using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using Microsoft.AspNet.SignalR;
 
 namespace Spades.Hubs
 {
     public class ChatHub : Hub
     {
-        public void Send(string username, string message)
+        public void Send(string username, string message, string email)
         {
-            Clients.All.addMessage(username ?? "unknown", message);
+            var md5 = MD5.Create();
+            var emailBytes = Encoding.ASCII.GetBytes(email);
+            var hash = md5.ComputeHash(emailBytes);
+
+            var emailHash = new StringBuilder();
+            foreach (var b in hash)
+            {
+                emailHash.Append(b.ToString("X2"));
+            }
+            
+            Clients.All.addMessage(username ?? "unknown", message, emailHash.ToString().ToLower());
         }
     }
 }
--- spades/spades/Scripts/index.js
+++ spades/spades/Scripts/index.js
@@ -1,30 +1,50 @@
+//var chatModel = function() {
+//    var self = this;
+//    //observables
+//    self.username = ko.observable();
+//    self.email = ko.observable();
+//    self.message = ko.observable();
+//    self.messages = ko.observableArray();
+    
+//    //computeds
+//    self.emailHash = ko.computed(function() {
+//        return CryptoJS.MD5(self.email().toLowerCase());
+//    });
+//};
+
 var chatModel = {
+    //observables
     username: ko.observable(),
+    email: ko.observable(),
     message: ko.observable(),
     messages: ko.observableArray(),
-    addMessage: function (user, message) {
-        this.messages.push({ username: user, message: message });
-    }
+
+    //computeds
+    //emailHash: ko.computed(function () {
+    //    return CryptoJS.MD5(this.email().toLowerCase());
+    //})
 };
 
 var chatHub = (function () {
     var server = $.connection.chatHub.server;
     var client = $.connection.chatHub.client;
 
-    client.addMessage = function(user, message) {
-        chatModel.addMessage(user, message);
+    client.addMessage = function(username, message, emailHash) {
+        chatModel.messages.push({ username: username, message: message, emailHash: emailHash });
     };
     
     return {
         send: function() {
-            server.send(chatModel.username(), chatModel.message());
+            server.send(chatModel.username(), chatModel.message(), chatModel.email());
             chatModel.message('');
         }
     };
 })();
 
 function enterRoom() {
-    $("#login-form").fadeOut(400, function () { $("#chat-area").fadeIn(); });
+    $("#login-form").fadeOut(400, function() {
+         $("#chat-area").fadeIn();
+    });
 }
 
 //page initialization
